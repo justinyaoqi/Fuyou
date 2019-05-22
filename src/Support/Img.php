248f1d5fe858c70714622f85384b9b1517f2364e
Yao Qi <yaoqiqq@hotmail.com>
2019-05-22 15:36:16 +0800

    Update Img.php

--- src/Support/Img.php
+++ src/Support/Img.php
@@ -9,9 +9,9 @@ class Img
     public static function base64EncodeImage($image_file)
     {
         $base64_image = '';
-        $image_info = getimagesize($image_file);
+        //$image_info = getimagesize($image_file);
         $image_data = fread(fopen($image_file, 'r'), filesize(trim($image_file)));
-        $base64_image = 'data:' . $image_info['mime'] . ';base64,' . chunk_split(base64_encode($image_data));
+        $base64_image = base64_encode($image_data); //'data:' . $image_info['mime'] . ';base64,' . chunk_split(base64_encode($image_data));
         return $base64_image;
     }
 }
